c18fd689b28f3d5c77ef58ec40f60b0f45ff9446
Michelle Wong <chellewsa@gmail.com>
2021-04-26 05:05:15 -0400

    spacing and misc. configs

--- src/main/java/com/miiwo/itemsonsale/ItemsOnSaleApplication.java
+++ src/main/java/com/miiwo/itemsonsale/ItemsOnSaleApplication.java
@@ -3,11 +3,11 @@ package com.miiwo.itemsonsale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ItemsOnSaleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemsOnSaleApplication.class, args);
 	}
-
 }
